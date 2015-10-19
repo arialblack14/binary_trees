ab18fcf23a422d9aac58176dc85908a3cd78442f
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-19 14:18:12 +0300

    Added built_tree method

--- binary.rb
+++ binary.rb
@@ -1,21 +1,39 @@
-# http://www.mikeperham.com/2014/11/26/building-a-binary-tree-with-enumerable/
-
 class Node
-	attr_accessor :data, :left, :right, :arr
+	attr_reader :data
+	attr_accessor :left, :right
 
 	def initialize data
 		@data = data
-		@arr = []
 	end
 
-	def build_tree arr
-		arr.each do |element|
-			element = data.new
+	def insert_in value
+		if value < @data
+			if @left.nil?
+				@left = Node.new(value)
+			else
+				@left = @left.insert_in(value)
+			end
+		else
+			if @right.nil?
+				@right = Node.new(value)
+			else
+				@right = @right.insert_in(value)
+			end
 		end
+	end	
+	
+	def build_tree arr
+		return nil if arr.length ==0
+		mid = arr.length/2
+
+		node = Node.new(arr[mid])
+
+		node.left = build_tree(arr[0..mid-1])
+		node.right = build_tree(arr[mid+1..-1])	
+		node
 	end
 end
 
 # Examples
-root = Node.new(7)
-root.left = Node.new(3)
-root.right = Node.new(12)
+test_array =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+puts build_tree(test_array)

3aa5af493e0b43643987a956327de81bde963593
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-20 02:05:47 +0300

    Tired of bfs/dfs...

--- binary.rb
+++ binary.rb
@@ -1,39 +1,111 @@
-class Node
-	attr_reader :data
-	attr_accessor :left, :right
+class BinaryTree
 
-	def initialize data
-		@data = data
-	end
+    def initialize(root = nil)
+        @root = root    
+    end
+    
+    def build_tree(a)
+        return nil unless a.is_a?(Array) && !a.empty?
+        
+        #shuffle the data to avoid a tree that is already sorted
+        data = a.shuffle
 
-	def insert_in value
-		if value < @data
-			if @left.nil?
-				@left = Node.new(value)
-			else
-				@left = @left.insert_in(value)
-			end
-		else
-			if @right.nil?
-				@right = Node.new(value)
-			else
-				@right = @right.insert_in(value)
-			end
-		end
-	end	
-	
-	def build_tree arr
-		return nil if arr.length ==0
-		mid = arr.length/2
-
-		node = Node.new(arr[mid])
-
-		node.left = build_tree(arr[0..mid-1])
-		node.right = build_tree(arr[mid+1..-1])	
-		node
-	end
+        @root = Node.new(data.shift)
+        
+        until data.empty? do
+            add_node(data.shift)
+        end
+    end
+   
+    def add_node(v)
+        node = Node.new(v)
+        current = @root
+        node_placed = false
+        
+        until node_placed do
+            if node.value < current.value
+                if current.lchild
+                    current = current.lchild 
+                else
+                    current.lchild = node
+                    node.parent = current
+                    node_placed = true
+                end
+            else
+                if current.rchild
+                    current = current.rchild 
+                else
+                    current.rchild = node
+                    node.parent = current
+                    node_placed = true
+                end
+            end
+        end
+    end
+   
+    def breadth_first_search(v)
+        queue = [@root]
+        
+        until queue.empty? do
+            current = queue.shift
+            return current if current.value == v
+            queue << current.lchild if current.lchild
+            queue << current.rchild if current.rchild
+        end
+        nil
+    end
+    
+    def depth_first_search(v)
+        stack = [@root]
+        
+        until stack.empty? do
+           current = stack.pop
+           return current if current.value == v
+           stack << current.rchild if current.rchild
+           stack << current.lchild if current.lchild
+        end
+        nil
+    end
+    
+    def dfs_rec(v)
+        dfs_rec_helper(v, @root)
+    end
+    
+    def dfs_rec_helper(v, current = nil)
+        #base cases
+        return nil if current.nil?
+        return current if current.value == v
+        
+        #check the left and right children if they exist
+        left_check = dfs_rec_helper(v, current.lchild) if current.lchild
+        right_check = dfs_rec_helper(v, current.rchild) if current.rchild
+        
+        #return the left or right check if they were successful
+        return left_check if left_check
+        return right_check if right_check
+        
+        #return nil if the value was not found
+        nil
+    end
+    
+    class Node
+        attr_accessor(:value, :parent, :lchild, :rchild)
+      
+        def initialize(value, parent = nil, lchild = nil, rchild = nil)
+            @value = value
+            @parent = parent
+            @lchild = lchild
+            @rchild = rchild
+        end
+    end
 end
 
-# Examples
-test_array =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-puts build_tree(test_array)
+#tests
+a = (1..10).to_a.shuffle
+
+b = BinaryTree.new
+b.build_tree(a)
+
+b.breadth_first_search(8)
+b.depth_first_search(3)
+b.dfs_rec(2)
